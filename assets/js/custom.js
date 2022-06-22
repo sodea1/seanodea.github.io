99b6dd947e9f32286b9016e2bbd5cb600bc09e2c
seanodea <seanodea11@gmail.com>
2022-06-22 13:09:27 -0400

    first

---
+++ assets/js/custom.js
@@ -0,0 +1,18 @@
+
+// document.querySelectorAll('a[href^="#one"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
+
+$(document).on('click', 'a[href^="#one"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
